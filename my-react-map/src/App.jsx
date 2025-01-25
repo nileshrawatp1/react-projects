f25a5b6b023a8af00c5445caadadee7d0ffe3603
nileshrawatp1 <33788262+nileshrawatp1@users.noreply.github.com>
2025-01-25 16:26:57 +0530

    Maps project added

---
+++ my-react-map/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './components/navbar'
+import Map from './components/map'
+
+function App() {
+
+    return (
+        <div className="App">
+            <Navbar />
+            <Map />
+        </div>
+    )
+}
+
+export default App
